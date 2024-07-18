f747d68146aefd808e76318901777ff894a3321c
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-07-17 20:04:24 -0700

    gaps test

--- assets/rtgraph/src/test/combine.test.ts
+++ assets/rtgraph/src/test/combine.test.ts
@@ -61,7 +61,7 @@ describe('interleave with gaps', function () {
         {Pos: 2, Timestamps: [30, 40, 70], Values: [3, 6, 9]},
     ]);
 
-    const expected = [
+    expect(cache.getData()).to.deep.equal([
         [new Date(10), 1, null, null],
         [new Date(30), null, null, 3],
         [new Date(39), NaN, null, null],
@@ -71,7 +71,29 @@ describe('interleave with gaps', function () {
         [new Date(70), 7, null, 9],
         [new Date(79), null, NaN, null],
         [new Date(80), null, 8, null],
-    ];
+    ]);
 
-    expect(cache.getData()).to.deep.equal(expected);
+    cache.append([
+        {Pos: 0, Timestamps: [90, 100], Values: [10, 11]},
+        {Pos: 1, Timestamps: [90, 140], Values: [12, 13]},
+        {Pos: 2, Timestamps: [100, 110], Values: [14, 15]},
+    ])
+
+    expect(cache.getData()).to.deep.equal([
+        [new Date(10), 1, null, null],
+        [new Date(30), null, null, 3],
+        [new Date(39), NaN, null, null],
+        [new Date(40), 4, 2, 6],
+        [new Date(50), null, 5, null],
+        [new Date(69), NaN, null, NaN],
+        [new Date(70), 7, null, 9],
+        [new Date(79), null, NaN, null],
+        [new Date(80), null, 8, null],
+        [new Date(90), 10, 12, null],
+        [new Date(99), null, null, NaN],
+        [new Date(100), 11, null, 14],
+        [new Date(110), null, null, 15],
+        [new Date(139), null, NaN, null],
+        [new Date(140), null, 13, null]
+    ]);
 });
